e39e2781ea25a3648b484abdd3916fcdea3861b6
me <1003586303@qq.com>
2018-05-09 22:41:58 +0800

    change db name

--- services/Scene_Service/entities/initial.go
+++ services/Scene_Service/entities/initial.go
@@ -9,7 +9,7 @@ import (
 var mydb *sql.DB
 
 func init() {
-	db, err := sql.Open("mysql", "root:fong@tcp(127.0.0.1:3306)/mydb?charset=utf8&parseTime=true")
+	db, err := sql.Open("mysql", "root:root@tcp(127.0.0.1:3306)/youhome?charset=utf8&parseTime=true")
 	if err != nil {
 		panic(err)
 	}
