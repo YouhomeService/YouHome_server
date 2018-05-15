32f62a8216f8f4cfcd18bfb011c283b04beee378
me <1003586303@qq.com>
2018-05-15 14:07:24 +0800

    db host

--- services/Gateway/service/config.go
+++ services/Gateway/service/config.go
@@ -7,7 +7,7 @@ var deviceService = host + ":9092"
 var sceneService = host + ":9094"
  */
 
-var host = "http://127.0.0.1"
-var userService = host + ":9091"
-var deviceService = host + ":9092"
+//var host = "http://127.0.0.1"
+var userService = "172.22.16.5:9091"
+var deviceService ="172.22.16.4:9092"
 var sceneService = "172.22.16.3:9093"
